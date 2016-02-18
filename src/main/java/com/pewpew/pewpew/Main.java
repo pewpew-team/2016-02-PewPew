c3cacc23f208a6696efedba270d08ece01d4a09a
Dmitry <dmitryrudneff@gmail.com>
2016-02-19 01:26:26 +0300

    Hotfix. Changed gitignore, deleted some unnecessary files.

--- src/main/java/com/pewpew/pewpew/Main.java
+++ src/main/java/com/pewpew/pewpew/Main.java
@@ -3,10 +3,6 @@ package com.pewpew.pewpew;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
-/**
- * Created by Leman on 18.02.16.
- */
-
 public class Main {
     public static void main(String[] args) {
         Server server = new Server(8080);
--- src/main/java/com/pewpew/pewpew/servelets/RegistrationService.java
+++ src/main/java/com/pewpew/pewpew/servelets/RegistrationService.java
@@ -1,8 +1,5 @@
 package com.pewpew.pewpew.servelets;
 
-/**
- * Created by Leman on 18.02.16.
- */
 public class RegistrationService {
 
 }
