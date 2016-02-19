72b1cb2483febfaf4753fec25b5d7a84ba822fa3
Alex Lavrinenko <aleksiosdev@gmail.co>
2016-02-19 14:47:00 +0300

    Add Test to DataBase

--- src/main/java/com/pewpew/pewpew/main/Main.java
+++ src/main/java/com/pewpew/pewpew/main/Main.java
@@ -2,13 +2,18 @@ package com.pewpew.pewpew.main;
 
 import com.pewpew.pewpew.model.User;
 import com.pewpew.pewpew.mongo.MongoModule;
+import com.pewpew.pewpew.servelet.RegistrationService;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        RegistrationService registrationService = new RegistrationService();
+        context.addServlet(new ServletHolder(registrationService),"/register");
         server.setHandler(context);
         MongoModule mongoModule = MongoModule.getInstanse();
         server.start();
@@ -22,3 +27,7 @@ public class Main {
         return user;
     }
 }
+
+// curl -H "Content-Type: application/json" -X POST -d '{"email":"xyz","password":"xyz"}' http://localhost:8080/register
+
+
---
+++ src/test/java/com/pewpew/pewpew/TestDataBase.java
@@ -0,0 +1,44 @@
+package com.pewpew.pewpew;
+
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.pewpew.pewpew.model.User;
+import com.pewpew.pewpew.mongo.MongoModule;
+import org.junit.Test;
+import com.pewpew.pewpew.additional.RandomString;
+import org.mongodb.morphia.query.Query;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+public class TestDataBase {
+    private MongoModule mongoModule;
+
+    public TestDataBase() {
+        this.mongoModule = MongoModule.getInstanse();
+    }
+
+    @Test()
+    public void creationTest() {
+        User user = new User();
+        RandomString randomString = new RandomString(10);
+        user.setPassword(randomString.nextString());
+        user.setEmail(randomString.nextString());
+
+        List<User> sameUser =  mongoModule.provideDatastore().find(User.class, "email", user.getEmail()).asList();
+
+        assertFalse(sameUser.size() != 0);
+
+        mongoModule.provideDatastore().save(user);
+        assertNotNull("Не назначается идентификатор", user.getId());
+        assertNotNull("Не создалось поле емеил", user.getEmail());
+        assertNotNull("Не создалось поле пароль", user.getPassword());
+    }
+
+
+}
